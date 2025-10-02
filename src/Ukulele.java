50cba5c2674c2c1af7d063132c8cb845622a20ce
AndreyLima-066 <andreyalmeidalima066@gmail.com>
2025-10-02 13:58:17 -0300

    Update Ukulele.java

--- src/Ukulele.java
+++ src/Ukulele.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class Ukulele {
+public class Ukulele extends Instrumento {
+    @Override
+    public void visitar() {
+        System.out.println("Tlim-tlim" + " Plim-plim" + " Tcham-tcham" + " Trim-trim" + " Dlim-dlim");
+    }
 }
