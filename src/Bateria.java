bf1f7964e5fa612267f799f0bd51676bf6cba3ac
AndreyLima-066 <andreyalmeidalima066@gmail.com>
2025-10-02 13:59:59 -0300

    Update Bateria.java

--- src/Bateria.java
+++ src/Bateria.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class Bateria {
+public class Bateria  extends Instrumento{
+    @Override
+    public void visitar() {
+        System.out.println("Tup-tup-tup" + " Bum-bum!" + " Pá!");
+    }
 }
